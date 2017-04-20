5fa1b55aff11d6b6e406edb90b7358965733c9b8
gems download installer <mhamidshaikh97@gmail.com>
2017-04-20 13:05:30 +0500

    articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+class ArticlesController < ApplicationController
+  def new
+  end
+end
+
+
+def create
+  render plain: params[:article].inspect
 end
